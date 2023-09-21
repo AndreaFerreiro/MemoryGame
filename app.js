e3eb366ba8bf38a27165eeb768b1e50624b896e9
“AndreaFerreiro” <andreafga28@gmail.com>
2023-09-21 12:46:33 +0200

    finish cards

--- app.js
+++ app.js
@@ -49,8 +49,7 @@ document.addEventListener('DOMContentLoaded', () => {
             name:'jiraf',
             img: 'images/jirafa.avif'
         }
-    ] 
-
+    ]
     cardArray.sort(() => 0.5 - Math.random())
     var cardsChoosen = []
     var cardsChoosenId = []
@@ -74,17 +73,17 @@ document.addEventListener('DOMContentLoaded', () => {
         const optoinTwoId = cardsChoosenId[1]
         if (cardsChoosen[0] === cardsChoosen[1]){
             alert ('Has encontrado una coincidencia!')
-            cards[optoinOneId].setAttribute('src','images/blanco.jpeg')
-            cards[optoinTwoId].setAttribute ('src', 'images/blanco.jpeg')
+            card[optoinOneId].setAttribute('src','images/blanco.jpeg')
+            card[optoinTwoId].setAttribute ('src', 'images/blanco.jpeg')
             cardsWon.push(cardsChoosen)
+            resultDisplay.textContent = cardsWon.length/2
         } else {
-            cards[optoinOneId].setAttribute('src','images/carta.jpeg')
-            cards[optoinTwoId].setAttribute('src','images/carta.jpeg')
+            card[optoinOneId].setAttribute('src','images/carta.jpeg')
+            card[optoinTwoId].setAttribute('src','images/carta.jpeg')
             alert ('Lo siento! Inténtalo de nuevo.')
         }
         cardsChoosen = []
         cardsChoosenId = []
-        resultDisplay.textContent = cardsWon.length
         if (cardsWon.length === cardArray.length/2){
             resultDisplay.textContent = 'Enhorabuena! Has encontrado todas las parejas!'
         }
@@ -92,12 +91,11 @@ document.addEventListener('DOMContentLoaded', () => {
     function flipcard (ev) {
         ev.preventDefault()
         var cardId = this.getAttribute('data-id')
-        cardChoosen.push(cardArray[cardId].name)
+        cardsChoosen.push(cardArray[cardId].name)
         cardsChoosenId.push(cardId)
         this.setAttribute('src', cardArray[cardId].img)
         if (cardsChoosen.length === 2 ){
             setTimeout (checkForMatch, 500)
         }
     }
-
 })
